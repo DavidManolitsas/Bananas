31715842573d2a4756c57f691749699f90c8c9b6
David Manolitsas <davidmanolitsas@icloud.com>
2020-08-06 15:15:06 +1000

    Removed random line used for testing

--- a1-s3763636_s3779380_s3557535_s3730303/AppDelegate.swift
+++ a1-s3763636_s3779380_s3557535_s3730303/AppDelegate.swift
@@ -12,8 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    var random:Int = 7
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
