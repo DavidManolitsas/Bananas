edc523f67f971bc7985fa55ca6aac259186c6122
Winnie <s3557535@student.rmit.edu.au>
2020-08-06 15:35:39 +1000

    Just put a nonsense variable in viewController

--- a1-s3763636_s3779380_s3557535_s3730303/ViewController.swift
+++ a1-s3763636_s3779380_s3557535_s3730303/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var hello = "hello"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
