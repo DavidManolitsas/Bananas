d85406ffafbbfd4598de0497d5bb3f903a374b45
David Manolitsas <davidmanolitsas@icloud.com>
2020-08-13 13:10:37 +1000

    Added MVVM Structure

---
+++ a1-s3763636_s3779380_s3557535_s3730303/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  a1-s3763636_s3779380_s3557535_s3730303
+//
+//  Created by David Manolitsas on 13/8/20.
+//  Copyright © 2020 David Manolitsas. All rights reserved.
+//
+
+import Foundation
+
+// This file is just a sample to add the Model folder to Git
+// Delete once another file has been added to the model group
