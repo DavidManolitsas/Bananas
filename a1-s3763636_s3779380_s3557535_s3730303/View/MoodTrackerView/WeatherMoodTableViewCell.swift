f9a97af2c70bdb756a81b386b902bc8c03c8332f
Jess C <s3763636@student.rmit.edu.au>
2020-08-20 11:19:28 +1000

    added weather icons and tableview cells

---
+++ a1-s3763636_s3779380_s3557535_s3730303/View/MoodTrackerView/WeatherMoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherMoodTableViewCell.swift
+//  a1-s3763636_s3779380_s3557535_s3730303
+//
+//  Created by Jess Cui on 20/8/20.
+//  Copyright © 2020 David Manolitsas. All rights reserved.
+//
+
+import UIKit
+
+class WeatherMoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
