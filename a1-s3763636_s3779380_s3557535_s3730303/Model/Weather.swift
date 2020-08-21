084af0b26612084531b75b67de9ff2f880a30343
Jess C <s3763636@student.rmit.edu.au>
2020-08-21 18:47:27 +1000

    updated mock weather and forecast objects

--- a1-s3763636_s3779380_s3557535_s3730303/Model/Weather.swift
+++ a1-s3763636_s3779380_s3557535_s3730303/Model/Weather.swift
@@ -1,3 +1,4 @@
+//
 //  Weather.swift
 //  a1-s3763636_s3779380_s3557535_s3730303
 //
@@ -7,38 +8,19 @@
 
 import Foundation
 
-struct Forecast {
-    private var main: String, description: String, iconName: String, maxTemp: Double, minTemp: Double
+struct Weather {
+    var forecasts: [Forecast] = []
     
-    init(main: String, description: String, iconName: String, maxTemp: Double, minTemp: Double) {
-        self.main = main
-        self.description = description
-        self.maxTemp = maxTemp
-        self.minTemp = minTemp
-        self.iconName = iconName
+    mutating func getForecasts() -> [Forecast] {
+        let clouds = Forecast(main: "Clouds", description: "scattered clouds", iconName: "03d", maxTemp: 12.14, minTemp: 7.12)
+        let clouds2 = Forecast(main: "Clouds", description: "broken clouds", iconName: "04d", maxTemp: 15.14, minTemp: 8.12)
+        let clear = Forecast(main: "Clear", description: "clear sky", iconName: "01d", maxTemp: 14.82, minTemp: 10.03)
+        let rain = Forecast(main: "Rain", description: "moderate rain", iconName: "10d", maxTemp: 11.93, minTemp: 6.32)
+        let thunder = Forecast(main: "Thunder", description: "thunder storms", iconName: "11d", maxTemp: 10.93, minTemp: 8.73)
         
-    }
-    
-    func getIconName() -> String {
-        return iconName
+        forecasts.append(contentsOf: [clouds, clouds2, clear, rain, thunder])
+        //print(mockForecasts)
+        //print(mockForecasts[3].getIconName())
+        return forecasts
     }
 }
-
-func populateMockObjects() -> [Forecast] {
-    var mockForecasts: [Forecast] = []
-    let clouds = Forecast(main: "Clouds", description: "scattered clouds", iconName: "03d", maxTemp: 12.14, minTemp: 7.12)
-    let clouds2 = Forecast(main: "Clouds", description: "broken clouds", iconName: "04d", maxTemp: 15.14, minTemp: 8.12)
-    let clear = Forecast(main: "Clear", description: "clear sky", iconName: "01d", maxTemp: 14.82, minTemp: 10.03)
-    let rain = Forecast(main: "Rain", description: "moderate rain", iconName: "10d", maxTemp: 11.93, minTemp: 6.32)
-    let thunder = Forecast(main: "Thunder", description: "thunder storms", iconName: "11d", maxTemp: 10.93, minTemp: 8.73)
-    
-    mockForecasts.append(contentsOf: [clouds, clouds2, clear, rain, thunder])
-    
-    return mockForecasts
-}
-
-
-
-
-//print(mockForecasts)
-//print(mockForecasts[3].getIconName())
