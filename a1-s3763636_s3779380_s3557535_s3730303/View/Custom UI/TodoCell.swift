620c1f593a8ed48306f2551e26ea72091bcc1b21
David Manolitsas <davidmanolitsas@icloud.com>
2020-08-23 18:20:17 +1000

    added checkbox to todo list

---
+++ a1-s3763636_s3779380_s3557535_s3730303/View/Custom UI/TodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  a1-s3763636_s3779380_s3557535_s3730303
+//
+//  Created by David Manolitsas on 23/8/20.
+//  Copyright © 2020 David Manolitsas. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
