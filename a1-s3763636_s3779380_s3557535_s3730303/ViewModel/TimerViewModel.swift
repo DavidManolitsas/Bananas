4fa39ceeb4d5cb880cbdc00eb5f14498a8684f2b
Winnie <s3557535@student.rmit.edu.au>
2020-09-15 13:24:50 +1000

    create a view model and model class for records

---
+++ a1-s3763636_s3779380_s3557535_s3730303/ViewModel/TimerViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  TimerViewModel.swift
+//  a1-s3763636_s3779380_s3557535_s3730303
+//
+//  Created by Winnie Siwan on 15/9/20.
+//  Copyright © 2020 David Manolitsas. All rights reserved.
+//
+
+import Foundation
+
+struct TimerViewModel {
+//    var studyRecords = [records]()
+    var timerRecords = TimerRecords()
+    
+    public mutating func updateRecord(breakduration: Int, alarmName: String, durations: Int){
+       timerRecords.updateRecord(breakduration: breakduration, alarmName: alarmName, durations: durations)
+    }
+  
+    
+   
+}
