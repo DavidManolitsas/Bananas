ad52446c9fcdb35af0f4ad18b1f0270f4258ef4f
kerwinxp <s3730303@student.rmit.edu.au>
2020-10-01 13:51:47 +1000

    add core data

---
+++ Records+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Records+CoreDataClass.swift
+//  
+//
+//  Created by kerwin on 1/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Records)
+public class Records: NSManagedObject {
+
+}
