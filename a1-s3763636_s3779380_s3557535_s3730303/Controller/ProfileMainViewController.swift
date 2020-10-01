55648b958241d657c46fd3164d90aae620d06eda
Winnie <s3557535@student.rmit.edu.au>
2020-10-02 00:21:14 +1000

    added adaptive layout to profile page pt2

--- a1-s3763636_s3779380_s3557535_s3730303/Controller/ProfileMainViewController.swift
+++ a1-s3763636_s3779380_s3557535_s3730303/Controller/ProfileMainViewController.swift
@@ -37,10 +37,16 @@ class ProfileMainViewController: UIViewController {
         // add subview to content view
         contentView.addSubview(pageViewController.view)
         
+        let views: [String: Any] = ["pageView": pageViewController.view!]
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[pageView]-0-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[pageView]-0-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+        
         guard let startingViewController = detailViewControllerAt(index: currentViewControllerIndex) else{
             return
         }
-        
+
         pageViewController.setViewControllers([startingViewController], direction: .forward, animated: true)
     }
     
